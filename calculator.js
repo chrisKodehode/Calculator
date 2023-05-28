09744ea8d13ef18893ba1f04278679de5234a16d
haugrud123 <chrishaugrud111@gmail.com>
2023-05-28 20:12:20 +0200

    + Button click test js

---
+++ calculator.js
@@ -0,0 +1,6 @@
+// Declare and assign the html buttons to a variable
+const buttonZero = document.getElementById("number-0");
+
+buttonZero.addEventListener("click", (event) => {
+    console.log("0")
+})
