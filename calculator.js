dde55ae036ccfcd6e4182887efaa8e9b15b36d75
haugrud123 <chrishaugrud111@gmail.com>
2023-05-29 14:17:37 +0200

    + Declare buttons and assign to variables

--- calculator.js
+++ calculator.js
@@ -1,6 +1,22 @@
-// Declare and assign the html buttons to a variable
-const buttonZero = document.getElementById("number-0");
+/*
+    TODO: 
+    * Keypress for numbers & symbols
+*/
 
-buttonZero.addEventListener("click", (event) => {
-    console.log("0")
-})
+// Declare and assign the html buttons & display to a variable
+const divDisplay  =  document.getElementById("display");
+const numberZero  =  document.getElementById("number-0");
+const numberDot   =  document.getElementById("number-dot");
+const letterC     =  document.getElementById("letter-C");
+const signEquals  =  document.getElementById("sign-equals");
+const numberOne   =  document.getElementById("number-1");
+const numberTwo   =  document.getElementById("number-2");
+const numberThree =  document.getElementById("number-3");
+const numberFour  =  document.getElementById("number-4");
+const numberFive  =  document.getElementById("number-5");
+const numberSix   =  document.getElementById("number-6");
+const numberSeven =  document.getElementById("number-7");
+const numberEight =  document.getElementById("number-8");
+const numberNine  =  document.getElementById("number-9");
+
+// Conditional if a button is entered, display it to the screen
