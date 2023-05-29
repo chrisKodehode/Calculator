f22cdfa839ac870376092b0952027fcd336dbcf2
haugrud123 <chrishaugrud111@gmail.com>
2023-05-29 15:28:18 +0200

    + Display more numbers

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 /*
     TODO: 
     * Keypress for numbers & symbols
+    ! 27-28 max characters in display box
 */
 
 // Declare and assign the html buttons & display to a variable
@@ -22,7 +23,7 @@ const numberNine  =  document.getElementById("number-9");
 numberZero.addEventListener("click", function(event) {
     event = divDisplay
     if (event === 0) {
-        console.log("Zero here")
+        console.log("0 here")
     } else {
         event.textContent += 0;
     }
@@ -33,9 +34,97 @@ numberZero.addEventListener("click", function(event) {
 numberOne.addEventListener("click", function(event) {
     event = divDisplay
     if (event === 0) {
-        console.log("Zero here")
+        console.log("1 here")
     } else {
         event.textContent += 1;
+    }
+
+    divDisplay = event;
+})
+
+numberTwo.addEventListener("click", function(event) {
+    event = divDisplay
+    if (event === 0) {
+        console.log("2 here")
+    } else {
+        event.textContent += 2;
+    }
+
+    divDisplay = event;
+})
+
+numberThree.addEventListener("click", function(event) {
+    event = divDisplay
+    if (event === 0) {
+        console.log("3 here")
+    } else {
+        event.textContent += 3;
+    }
+
+    divDisplay = event;
+})
+
+numberFour.addEventListener("click", function(event) {
+    event = divDisplay
+    if (event === 0) {
+        console.log("4 here")
+    } else {
+        event.textContent += 4;
+    }
+
+    divDisplay = event;
+})
+
+numberFive.addEventListener("click", function(event) {
+    event = divDisplay
+    if (event === 0) {
+        console.log("5 here")
+    } else {
+        event.textContent += 5;
+    }
+
+    divDisplay = event;
+})
+
+numberSix.addEventListener("click", function(event) {
+    event = divDisplay
+    if (event === 0) {
+        console.log("6 here")
+    } else {
+        event.textContent += 6;
+    }
+
+    divDisplay = event;
+})
+
+numberSeven.addEventListener("click", function(event) {
+    event = divDisplay
+    if (event === 0) {
+        console.log("7 here")
+    } else {
+        event.textContent += 7;
+    }
+
+    divDisplay = event;
+})
+
+numberEight.addEventListener("click", function(event) {
+    event = divDisplay
+    if (event === 0) {
+        console.log("8 here")
+    } else {
+        event.textContent += 8;
+    }
+
+    divDisplay = event;
+})
+
+numberNine.addEventListener("click", function(event) {
+    event = divDisplay
+    if (event === 0) {
+        console.log("9 here")
+    } else {
+        event.textContent += 9;
     }
 
     divDisplay = event;
